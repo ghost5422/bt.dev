113424c0bf8708602eab26986d91fa8844f3aa07
Berkay <139920659+ghost5422@users.noreply.github.com>
2026-02-10 22:02:24 +0300

    script.js

--- script.js
+++ script.js
@@ -13,20 +13,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// WhatsApp Form
+// WhatsApp Form Fonksiyonu
 function whatsappGonder(e) {
     e.preventDefault();
     
+    // HTML'deki id="ad" ve id="konu" alanlarından veri alır
     const ad = document.getElementById('ad').value;
     const konu = document.getElementById('konu').value;
+    
+    // Telefon numarası (Başında + olmadan, ülke kodu ile)
     const numara = "905340188445"; 
 
     if(ad === "" || konu === "Genel") {
-        alert("Lütfen bilgileri doldurunuz.");
+        alert("Lütfen adınızı girin ve bir konu seçin.");
         return;
     }
 
+    // Mesajı oluştur
+    // %0A = Yeni satır
     const mesaj = `*WEB SİTESİNDEN MESAJ VAR!* %0A%0A👤 *İsim:* ${ad}%0A📝 *Konu:* ${konu}%0A%0AMerhaba Berkay Bey, hizmetleriniz hakkında görüşmek istiyorum.`;
     
+    // WhatsApp'ı yeni sekmede aç
     window.open(`https://wa.me/${numara}?text=${mesaj}`, '_blank');
 }
